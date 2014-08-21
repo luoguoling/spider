1e4b9bca964413f0f1752afdb1a1af15c065a92e
luoguoling <luoguoling1@gmail.com>
2014-08-21 06:35:36 -0700

    url1

---
+++ url1.py
@@ -0,0 +1,17 @@
+import urllib
+def callback(a,b,c):
+    """
+    @a:num
+    @b:large
+    @c:remote file large
+    """
+    down_progress = 100.0*a*b/c
+    if down_progress > 100:
+        down_progress = 100
+    print "%.2f%%" % down_progress
+    print a*b
+
+url = "http://www.python.org"
+urllib.urlretrieve(url,"c.html",callback)
+html = urllib.urlopen(url)
+print html.info()
