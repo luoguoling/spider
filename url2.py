1227ab2ded769a1c271d8fb2c044b5d651073e4c
luoguoling <luoguoling1@gmail.com>
2014-08-22 02:01:21 -0700

    Exception

---
+++ url2.py
@@ -0,0 +1,18 @@
+from urllib2 import Request,urlopen,URLError,HTTPError
+def url2():
+    url = "http://www.baidu7784.com/"
+    req = Request(url)
+    try:
+        response = urlopen(req)
+        print response.read()
+        print response.getcode()
+    except URLError,e:
+        if hasattr(e,'code'):
+            print 'the server can not achiee'
+            print 'Error code',e.code
+        elif hasattr(e,'reason'):
+            print 'Error reason',e.reason
+        else:
+            print 'no exception'
+    
+url2()
